d523cc8e70f65eeb65e5a597e689e51be72d8dd3
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-06-24 19:07:49 -0600

    add working notification structure

--- App.js
+++ App.js
@@ -1,19 +1,85 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react'
+import { StyleSheet, Text, View, Button, Alert, Linking, AppState } from 'react-native'
+import { Notifications } from 'expo'
+import * as Permissions from 'expo-permissions'
+import Constants from 'expo-constants'
+import useAppState from 'react-native-appstate-hook'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const App = () => {
+	const [notificationPermission, setNotificationPermission] = useState(false)
+	const [token, setToken] = useState(null)
+	const [preferencesClicked, setPreferencesClicked] = useState(false)
+
+	const { appState } = useAppState({
+		onForeground: async () => {
+			if (preferencesClicked) {
+				getPermission()
+				setToken(null)
+				setPreferencesClicked(false)
+			}
+		},
+	})
+
+	useEffect(() => {
+		checkPermission()
+	}, [checkPermission, notificationPermission])
+
+	const checkPermission = useCallback(async () => {
+		if (Constants.isDevice) {
+			const permission = await getPermission()
+			if (permission === 'granted') {
+				try {
+					const expoToken = await Notifications.getExpoPushTokenAsync()
+					setToken(expoToken)
+				} catch (error) {
+					console.log(error)
+				}
+			} else {
+				updatePermission()
+			}
+		} else {
+			Alert.alert('Sorry!', 'You must use a physical device to receive Push Notifications.')
+		}
+	}, [updatePermission])
+
+	const getPermission = async () => {
+		const existingStatus = await Permissions.getAsync(Permissions.NOTIFICATIONS)
+		return setNotificationPermission(existingStatus.granted)
+	}
+
+	const updatePermission = useCallback(() => {
+		Alert.alert(
+			'Notification Permission',
+			'If you would like to be able to receive Push Notifications, you must change them in your app settings.',
+			[
+				{ text: 'Cancel', onPress: null, style: 'cancel' },
+				{
+					text: 'Continue',
+					onPress: () => {
+						Linking.openURL('app-settings:')
+						setPreferencesClicked(true)
+					},
+				},
+			],
+		)
+	}, [])
+
+	return (
+		<View style={styles.container}>
+			<Text>App state: {appState}</Text>
+			<Text>Here's the token maybe: {token}</Text>
+			<Button onPress={updatePermission} title='Get Permission Again' />
+		</View>
+	)
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
