73801303933b1e19d897075d69f48d3d1b5e527e
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2021-01-04 19:38:31 -0700

    working with SectionList

--- components/ContactExample.js
+++ components/ContactExample.js
@@ -1,11 +1,12 @@
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import * as React from 'react'
+
+import { SectionList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 import { pull } from 'lodash'
 import { useLocalContacts } from '../data/useLocalContacts'
 
 const Item = ({ item, selectedData, setSelectedData }) => {
-	const [selected, setSelected] = useState(selectedData.includes(item))
+	const [selected, setSelected] = React.useState(selectedData.includes(item))
 	const addSelected = () => setSelectedData([...selectedData, item])
 	const removeSelected = () => setSelectedData(pull(selectedData, selected))
 	const onPress = () => {
@@ -26,7 +27,32 @@ const Item = ({ item, selectedData, setSelectedData }) => {
 
 export const ContactExample = () => {
 	const { status, data, error, isFetching } = useLocalContacts()
-	const [selectedData, setSelectedData] = useState([])
+	const [selectedData, setSelectedData] = React.useState([])
+	const [dataGroups, setDataGroups] = React.useState([])
+
+	React.useEffect(() => {
+		if (status !== 'loading' && status !== 'error') {
+			const groupContactsByLetter = () => {
+				const contactsByLetter = []
+				data.forEach(contact => {
+					const { firstName, lastName, company } = contact
+					const letter = lastName?.charAt(0) || firstName?.charAt(0) || company?.charAt(0)
+					// If letter doesn't exist in array, create it
+					!(letter in contactsByLetter) ? (contactsByLetter[letter] = { title: letter, data: [] }) : null
+					// Push contact to correct letter
+					contactsByLetter[letter].data.push(contact)
+				})
+				const sortedLetters = Object.keys(contactsByLetter).sort()
+				const sortedArray = []
+				sortedLetters.forEach(letter => {
+					const letterGroup = contactsByLetter[letter]
+					sortedArray.push(letterGroup)
+				})
+				return sortedArray
+			}
+			setDataGroups(groupContactsByLetter())
+		}
+	}, [data, status])
 
 	const renderPosts = () => {
 		if (status === 'loading') {
@@ -37,7 +63,24 @@ export const ContactExample = () => {
 			const renderItem = ({ item }) => {
 				return <Item item={item} selectedData={selectedData} setSelectedData={setSelectedData} />
 			}
-			return <FlatList data={data} keyExtractor={item => item.id} renderItem={renderItem} extraData={selectedData} />
+			const renderSectionHeader = ({ section: { title } }) => {
+				return (
+					title && (
+						<View style={styles.sectionHeader}>
+							<Text style={styles.sectionHeaderText}>{title}</Text>
+						</View>
+					)
+				)
+			}
+			return (
+				<SectionList
+					sections={dataGroups}
+					keyExtractor={item => item.id}
+					renderItem={renderItem}
+					renderSectionHeader={renderSectionHeader}
+					extraData={selectedData}
+				/>
+			)
 		}
 	}
 
@@ -58,5 +101,17 @@ const styles = StyleSheet.create({
 		paddingHorizontal: 10,
 		paddingVertical: 20,
 		marginVertical: 4,
+	},
+	sectionHeader: {
+		height: 30,
+		flexDirection: 'row',
+		alignItems: 'center',
+		padding: 10,
+	},
+	sectionHeaderText: {
+		fontWeight: 'bold',
+		textAlign: 'center',
+		width: 30,
+		height: 15,
 	},
 })
